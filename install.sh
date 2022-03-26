79b0a206959727ffbd60b4e3759a091f091a2430
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2022-03-26 14:36:48 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo fuck you
+if [[ -d "/usr/bin/python3" ]]; then
+    sudo apt update
+    sudo apt upgrade
+    sudo apt install Python
+    sudo apt install php
+    python3 -m pip install websocket-client
+    python3 -m pip install simple_http_server
+fi
+cd gex
+python3 -m http.server
