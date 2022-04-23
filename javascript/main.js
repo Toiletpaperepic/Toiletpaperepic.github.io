31f8f4f0b3d26f0b0261d7f83193429c1b80cceb
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2022-04-23 09:52:46 -0400

    cool

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,26 @@
+function fnBrowserDetect(){
+                 
+    let userAgent = navigator.userAgent;
+    let browserName;
+    
+    if(userAgent.match(/chrome|chromium|crios/i)){
+        browserName = "chrome";
+      }else if(userAgent.match(/firefox|fxios/i)){
+        pledge ()
+        browserName = "firefox";
+      }  else if(userAgent.match(/safari/i)){
+        browserName = "safari";
+      }else if(userAgent.match(/opr\//i)){
+        browserName = "opera";
+      } else if(userAgent.match(/edg/i)){
+        browserName = "edge";
+      }else{
+        browserName="No browser detection";
+      }
+             
+    console.log("You are using "+ browserName +" browser");
+}
+fnBrowserDetect()
 function pledge () {
     console.log("l");
     console.log("pledge");
@@ -31,3 +54,8 @@ function pledge () {
     console.log("for");
     console.log("all");
             }
+
+
+console.log("%ci stole amogos.studio html noob", "color:blue; font-size:50px");
+console.log("%c3yr old haccing skills #2: inspect elements and console", "color:blue; font-size:50px");
+console.log("%cstop being s u s you little baka", "color:red; font-size:80px");
