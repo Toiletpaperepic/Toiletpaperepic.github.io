de2c4b035e81fdd2555a14aac07a078e644e689b
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2022-04-29 22:05:29 -0400

    winamp
    Winamp 2 reimplemented for the browser

--- javascript/main.js
+++ javascript/main.js
@@ -89,6 +89,12 @@ function closeFullscreen() {
     document.msExitFullscreen();
   }
 }
+function winamp(){
+  const app = document.getElementById("app")
+  const webamp = new Webamp();
+  webamp.renderWhenReady(app);
+;}
+
 console.log("%ci stole amogos.studio html noob", "color:blue; font-size:50px");
 console.log("%c3yr old haccing skills #2: inspect elements and console", "color:blue; font-size:50px");
 console.log("%cstop being s u s you little baka", "color:red; font-size:80px");
